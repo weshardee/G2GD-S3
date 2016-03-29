37ca0c4779f7352f63a1378c1bbbc45e0cf67aea
Wes Hardee <weshardee@gmail.com>
2016-03-29 08:47:49 -0500

    cast a ray on shoot

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -23,10 +23,29 @@ public class Shoot : MonoBehaviour
 
     void Fire()
     {
-        if (Time.time > nextFire) {
-            // TODO fire
-            Debug.Log("shoot");
+        if (Time.time > nextFire)
+        {
+            // add firing delay
             nextFire = fireRate + Time.time;
+
+            // cast the ray
+            Vector3 origin = transform.position;
+            Vector3 direction = transform.forward;
+            RaycastHit hitInfo;
+            bool hit = Physics.Raycast(origin, direction, out hitInfo);
+
+            // exclude non-enemy hits
+            hit = hit && hitInfo.transform.CompareTag("Enemy");
+
+            // draw the debug ray
+            Color rayColor = hit ? Color.green : Color.red;
+            Debug.DrawRay(origin, direction, Color.green, 3f);
+
+            if (hit) {
+                Debug.Log(hitInfo.transform.name);
+
+                // TODO destroy the object
+            }
         }
     }
 }
