59db523bcabdd3b3f8336b7ace9dc37891bc316a
Wes Hardee <weshardee@gmail.com>
2016-03-29 23:13:49 -0500

    attach FindEnemies behavior

--- Assets/Scripts/FindEnemies.cs
+++ Assets/Scripts/FindEnemies.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class FindEnemies : MonoBehaviour
 {
+    private GameObject[] enemies;
+
     void Start()
     {
+        GoFindEm();
     }
 
     void Update()
     {
+    }
+
+    void GoFindEm() {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 }
